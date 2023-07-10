c8c07cb6531f12b03aa9748b27ad1ef9ec2940b1
y-hiroki-radiotech <bnndd439@ybb.ne.jp>
2023-07-10 20:04:05 +0900

    modify pfi class

--- permutation_feature_importance/PermutationFeatureImportance.py
+++ permutation_feature_importance/PermutationFeatureImportance.py
@@ -3,7 +3,8 @@ import numpy as np
 import pandas as pd
 from dataclasses import dataclass
 from typing import Any, Union, Callable, List
-from sklearn.metrics import mean_squared_error, mean_absolute_error, mean_squared_log_error, mean_absolute_percentage_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.metrics import mean_squared_log_error, mean_absolute_percentage_error
 import matplotlib.pyplot as plt
 import japanize_matplotlib
 
