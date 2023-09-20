e0118fb4933f1e1988258c011e4961870efe37c6
y-hiroki-radiotech <bnndd439@ybb.ne.jp>
2023-09-20 17:32:17 +0900

    造影を複数相を撮影したものがCTDIに足し合わされているものを解消

--- preprocessing/remove_duplicate_ctdi.py
+++ preprocessing/remove_duplicate_ctdi.py
@@ -2,6 +2,24 @@ import pandas as pd
 import numpy as np
 
 
+def split_aggregated_scans(df, accession_list):
+    """この関数は単純＋造影2相のようなスキャンの場合に、造影2相が加算されてしまうRDSRの現象に対応するもの
+    　　　　　　加算されてしまったデータに対して、1スキャンに分離するためのものです。
+    """
+    
+    protocol_list = ['5.10 Chest - Pelvis (40sec,80sec)Routine','5.11 Chest - Pelvis (40sec,80sec)Routine','5.12 Chest - Pelvis (40sec,80sec)Routine']
+    for accession in accession_list:
+        df_subset = df[df['accession'] == accession]
+        if df_subset['scan protocol'].unique()[0] in protocol_list:
+            min_ctdi = df_subset['Mean CTDIvol'].min()
+            max_ctdi = df_subset['Mean CTDIvol'].max()
+            if 1.5 < max_ctdi:
+                df_subset_index = df_subset.index
+                min_ctdi_index = df_subset[df_subset['Mean CTDIvol'] == min_ctdi].index
+                rest_index = list(set(df_subset_index) - set(min_ctdi_index))
+                df.loc[rest_index, 'Mean CTDIvol'] = df_subset.loc[rest_index]['Mean CTDIvol'] / len(df_subset.loc[rest_index])
+
+
 def remove_duplicate_ctdi(df):
     """この関数は、読み込んだデータフレームの複数回スキャンした検査のCTDIのうち最大値のスキャンデータを残します。
     　　　　　　単独スキャンデータにデータを結合したデータフレームを返す
@@ -27,6 +45,9 @@ def remove_duplicate_ctdi(df):
     
     # 重複データのaccessionの集合を作成
     duplicated_accession_set = set(df_duplicated['accession'])
+    
+    # 造影相の加算に対応するための処理を追加
+    split_aggregated_scans(df_duplicated, duplicated_accession_set)
     
     # 複数スキャンの最大値のindexのみ取得する
     max_indices = df_duplicated.groupby('accession')['Mean CTDIvol'].idxmax()
